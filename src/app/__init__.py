7a651cc093164aba522c0acaf723d68c34ca1d5b
TheFid12 <zhenya.bytchkov@mail.ru>
2025-11-11 16:48:20 +0300

    backend upd dockerfile+requ+for amvera

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,13 +1,23 @@
-from flask import Flask # type: ignore
+from flask import Flask, send_from_directory # type: ignore
 from flask_cors import CORS # type: ignore
 from src.app.admin import blueprints
+import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
     CORS(app)
 
     # Register all blueprints
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
+
+    @app.route('/docs/<path:filename>')
+    def serve_docs(filename):
+        docs_path = os.path.join(app.static_folder, 'docs')
+        return send_from_directory(docs_path, filename)
+
+    @app.route('/')
+    def index():
+        return send_from_directory(os.path.join(app.static_folder, 'docs'), 'index.html')
 
     return app
